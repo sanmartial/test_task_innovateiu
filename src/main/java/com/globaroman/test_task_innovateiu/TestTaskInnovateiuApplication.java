23baca18f9988577bb86dbedcbfdc54acdf0a7f1
Roman Globa <globaroman@gmail.com>
2024-11-20 20:48:19 +0200

    Create project

---
+++ src/main/java/com/globaroman/test_task_innovateiu/TestTaskInnovateiuApplication.java
@@ -0,0 +1,13 @@
+package com.globaroman.test_task_innovateiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskInnovateiuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskInnovateiuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/globaroman/test_task_innovateiu/TestTaskInnovateiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaroman.test_task_innovateiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskInnovateiuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
