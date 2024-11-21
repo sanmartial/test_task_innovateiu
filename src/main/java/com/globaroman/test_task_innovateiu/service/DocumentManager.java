eee0dd5bfec5c351d47e7c5558311779d56a6ad6
Roman Globa <globaroman@gmail.com>
2024-11-21 22:51:23 +0200

    The task was completed

--- src/main/java/com/globaroman/test_task_innovateiu/service/DocumentManager.java
+++ src/main/java/com/globaroman/test_task_innovateiu/service/DocumentManager.java
@@ -2,20 +2,30 @@ package com.globaroman.test_task_innovateiu.service;
 
 import lombok.Builder;
 import lombok.Data;
-
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+/**
+ * For implement this task focus on clear code, and make this solution as simple readable as possible
+ * Don't worry about performance, concurrency, etc
+ * You can use in Memory collection for sore data
+ * <p>
+ * Please, don't change class name, and signature for methods save, search, findById
+ * Implementations should be in a single class
+ * This class could be auto tested
+ */
+
 public class DocumentManager {
 
-    private List<Document> documents = new ArrayList<>();
+    private final List<Document> documents = new ArrayList<>();
 
     /**
      * Implementation of this method should upsert the document to your storage
@@ -36,12 +46,13 @@ public class DocumentManager {
             document.setId(generateUniqueId());
         }
 
+        documents.removeIf(d -> d.getId().equals(document.getId()));
+
         documents.add(document);
 
         return findById(document.getId()).orElseThrow(() ->
                 new RuntimeException("The document was not created. Try again."));
     }
-
 
     /**
      * Implementation this method should find documents which match with request
@@ -62,6 +73,27 @@ public class DocumentManager {
             documentList.addAll(docByAuthor);
         }
 
+        if (request.getContainsContents() != null) {
+            Set<Document> docByContent = documents.stream()
+                    .filter(d -> filterByContainsContents(d.getContent(), request.getContainsContents()))
+                    .collect(Collectors.toSet());
+            documentList.addAll(docByContent);
+        }
+
+        if (request.getTitlePrefixes() != null) {
+            Set<Document> docByTitle = documents.stream()
+                    .filter(d -> filterByTitlePrefixes(d.getTitle(), request.getTitlePrefixes()))
+                    .collect(Collectors.toSet());
+            documentList.addAll(docByTitle);
+        }
+
+        if (request.getCreatedFrom() != null && request.getCreatedTo() != null) {
+            Set<Document> docByData = documents.stream()
+                    .filter(d -> filterPeriodData(d.getCreated(), request.getCreatedFrom(), request.getCreatedTo()))
+                    .collect(Collectors.toSet());
+            documentList.addAll(docByData);
+        }
+
         return new ArrayList<>(documentList);
     }
 
@@ -76,7 +108,31 @@ public class DocumentManager {
     }
 
     private String generateUniqueId() {
-        return UUID.randomUUID().toString().replace("-", "").substring(0, 8);
+        return UUID.randomUUID()
+                .toString()
+                .replace("-", "")
+                .substring(0, 8);
+    }
+
+    private boolean filterPeriodData(Instant created, Instant createdFrom, Instant createdTo) {
+        return !created.isBefore(createdFrom) && !created.isAfter(createdTo);
+    }
+
+    private boolean filterByTitlePrefixes(String title, List<String> titlePrefixes) {
+        if (title == null || titlePrefixes.isEmpty()) {
+            return true;
+        }
+
+        return titlePrefixes.stream().anyMatch(title::startsWith);
+    }
+
+    private boolean filterByContainsContents(String content, List<String> containsContents) {
+        if (content == null || containsContents.isEmpty()) {
+            return true;
+        }
+
+        return containsContents.stream().anyMatch(c -> content.toLowerCase(Locale.ROOT)
+                .contains(c.toLowerCase(Locale.ROOT)));
     }
 
     @Data
